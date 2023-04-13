d2131e0425e62f7bcbdc612c92b6e03574217d26
Kara Brightwell <kara@153.io>
2023-04-13 09:05:58 +0100

    feat: parse origin url for github calls

--- src/pull-request.ts
+++ src/pull-request.ts
@@ -4,6 +4,7 @@ import { Octokit } from "@octokit/rest";
 import * as suggester from 'code-suggester'
 import path from "path";
 import { formatPRChangelog } from "./changelog";
+import parseGithubUrl from 'parse-github-url'
 
 function applyChanges(bump: PackageBump) {
 	for(const change of bump.changes) {
@@ -11,7 +12,7 @@ function applyChanges(bump: PackageBump) {
 	}
 }
 
-export async function createPullRequest(bumps: PackageBumps, { root, commit }: Context) {
+export async function createPullRequest(bumps: PackageBumps, { git, root, commit }: Context) {
 	const octokit = new Octokit({
 		auth: process.env.GITHUB_TOKEN
 	})
@@ -37,9 +38,21 @@ export async function createPullRequest(bumps: PackageBumps, { root, commit }: C
 		)
 	}
 
-	// TODO
-	const owner = 'Financial-Times'
-	const repo = 'evergiven-test'
+	const origin = await git.remote(['get-url', 'origin'])
+	if(!origin) {
+		throw new Error('git repo has no origin')
+	}
+
+	const parsed = parseGithubUrl(origin)
+	if(!parsed) {
+		throw new Error(`could not parse origin URL ${origin}`)
+	}
+
+	const { owner, repo } = parsed
+	if(!owner || !repo) {
+		throw new Error(`couldn't parse owner/repo from ${origin}`)
+	}
+
 	const branch = 'evergiven-release'
 
 	const { data: pulls } = await octokit.pulls.list({
